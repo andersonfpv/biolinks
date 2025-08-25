05c473ef480e977d4ae31c56706955c9d29a28cf
A12 <A12@NOTEA1208.santuario.local>
2025-08-25 11:58:58 -0300

    Validando formuláeio

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,27 @@
 <div>
     <h1>Login</h1>
+
+    @if($message = session()->get('message'))
+        <div>{{ $message }}</div>
+    @endif
+
     <form action="/login" method="post">
 
         @csrf
+        <div>
+            <input name="email" placeholder="Email" value="{{ old('email') }}"/>
+            @error('email')
+                <span>{{ $message }}</span>
+            @enderror
 
-        <input name="email" placeholder="Email" value="{{ old('email') }}"/>
-        <input type="password" name="password" placeholder="Senha"/>
+        <br><br>
+
+        <div>
+            <input type="password" name="password" placeholder="Senha"/>
+            @error('password')
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
 
         <button>Logar</button>
 
